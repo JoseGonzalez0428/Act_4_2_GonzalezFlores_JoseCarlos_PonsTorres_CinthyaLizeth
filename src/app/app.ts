dfb7f988a8f4d05d168a69eb3f460ca3791290d6
José González <josegf2004@gmail.com>
2026-05-07 15:25:58 -0600

    Se agregó Badge

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Label } from './Components/label/label';
 import { ProgressBar } from './Components/progress-bar/progress-bar';
+import { Badge } from './Components/badge/badge';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Label, ProgressBar],
+  imports: [RouterOutlet, Label, ProgressBar,Badge],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

1586008eb16d563b099ccd3ac5d6a71bb09937c2
José González <josegf2004@gmail.com>
2026-05-11 17:56:17 -0600

    Se agregaron iconos a botones

--- src/app/Components/button/button.ts
+++ src/app/Components/button/button.ts
@@ -1,14 +1,16 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IconComponent } from '../icon/icon';
 
 @Component({
   selector: 'app-button',
-  imports: [CommonModule],
+  imports: [CommonModule, IconComponent],
   templateUrl: './button.html',
   styleUrl: './button.css'
 })
 export class ButtonComponent {
   @Input() label: string = 'Button';
+  @Input() icon: string = 'add';
   @Input() variant: 'primary' | 'primary-hover' | 'primary-active' | 'secondary' | 'secondary-hover' | 'secondary-active' | 'disabled' | 'success' | 'warning' | 'error' = 'primary';
   @Input() showIcon: boolean = true;
   @Output() clicked = new EventEmitter<void>();
@@ -18,4 +20,20 @@ export class ButtonComponent {
       this.clicked.emit();
     }
   }
+
+  getIconColor(): string {
+  const colorMap: {[key: string]: string} = {
+    'primary': 'white',
+    'primary-hover': 'white',
+    'primary-active': 'white',
+    'secondary': 'blue',
+    'secondary-hover': 'blue',
+    'secondary-active': 'blue',
+    'disabled': 'disabled',
+    'success': 'green',
+    'warning': 'yellow',
+    'error': 'red'
+  };
+  return colorMap[this.variant] || 'white';
+}
 }
